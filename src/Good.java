f3e7c901986b017badf0a7adf3f2515b9b8713f8
thendralprasath <thendralprasath@gmail.com>
2021-07-27 16:09:18 +0530

    changes in god

--- src/Good.java
+++ src/Good.java
@@ -3,13 +3,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Good {
 	public static void main(String[] args) {
-	       System.setProperty("webdriver.chrome.driver","C:\\Users\\ARI\\Desktop\\eworkspace\\God\\driver\\chromedriver.exe");
-	       WebDriver driver=new ChromeDriver();
+		 System.setProperty("webdriver.chrome.driver","C:\\Users\\ARI\\Desktop\\chromee\\chromedriver.exe");
+	       ChromeDriver driver=new ChromeDriver();
 	       driver.get("https://www.facebook.com" );
 	       String ti=driver.getTitle();
 	       System.out.print(ti);
 	       
 	     String url=driver.getCurrentUrl();
 	       System.out.print(url);
+	       System.out.println("jkxbcjaidbfviebfighebrgfj");
+	       System.out.println("jkxbcjaidbfviebfighebrgfj");
+	       System.out.println("jkxbcjaidbfviebfighebrgfj");
+	       System.out.println("jkxbcjaidbfviebfighebrgfj");
+	       System.out.println("jkxbcjaidbfviebfighebrgfj");
+	       
 }
 }
